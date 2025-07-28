4e04d664d3ccda3ddc88d57f3bfcd6f90d14dc74
231274 <231274@CPD02-M13>
2025-07-28 10:53:06 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppFit/Helpers/SQLiteDataBaseHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppFit.Models;
+using SQLite;
+
+namespace MauiAppFit.Helpers
+{
+    public class SQLiteDataBaseHelper
+    {
+        readonly SQLiteAsyncConnection _db;
+
+        public SQLite
+    }
+}
---
+++ MauiAppFit/Models/Atividade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppFit.Models
+{
+    public class Atividade
+    {
+        [PrimaryKeyAttribute, AutoIncrementAttribute]
+        public int Id { get; set; }
+        public string? Descricao { get; set; }
+        public DataTime Data {  get; set; }
+        public double? Peso { get; set; }
+        public string? Observacoes { get; set; }
+    }
+}
